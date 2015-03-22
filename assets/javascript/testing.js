6f7cd3638c2062dd318056eec5edcaa03927af89
Gabe G'Sell <gabergg@gmail.com>
2015-03-22 01:31:42 -0400

    working test of all potential game paths

---
+++ assets/javascript/testing.js
@@ -0,0 +1,39 @@
+function alertToConsole() {
+    window.alert = function (text) {
+        $('body').data("alert", text);
+    }
+}
+
+var possiblePaths = []
+
+function runTest() {
+
+    alertToConsole();
+
+    $('.cell:empty').each(function () {
+        possiblePaths.push(this.id);
+    });
+
+    while (possiblePaths.length > 0) {
+        len = possiblePaths.length;
+        for (var i = 0; i < len; i++) {
+            $('body').data("alert", "");
+            for (var j = 0; !finishedUp && j < possiblePaths[i].length; j++) {
+                $('#' + possiblePaths[i][j]).trigger('click');
+                if ($('body').data("alert")) {
+                    if ($('body').data("alert") == "You win!") {
+                        console.error("Test Failed!", possiblePaths[i]);
+                    }
+                }
+            }
+            if (!$('body').data("alert")) {
+                $('.cell:empty').each(function () {
+                    possiblePaths.push(possiblePaths[i] + this.id);
+                });
+            }
+            resetBoard();
+        }
+        possiblePaths.splice(0, len);
+    }
+
+}
