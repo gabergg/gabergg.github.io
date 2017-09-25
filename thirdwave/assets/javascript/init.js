442e3533af53b1d9bb4ba454dd1094b2b63911fb
Gabe G'Sell <gabergg@gmail.com>
2017-09-25 00:10:14 -0700

    x

--- thirdwave/assets/javascript/init.js
+++ thirdwave/assets/javascript/init.js
@@ -8,19 +8,40 @@ function toggleUpdateForm(el) {
   }
 }
 
+const formHTML = `
+  <label for="name">name</label>
+  <input type="text" name="gender" id="name"><br>
+  <label for="location">location</label>
+  <input type="text" name="gender" id="location"><br>
+  <label for="description">description</label>
+  <input type="text" name="gender" id="description"><br>
+  <label for="thumbnail">thumbnail</label>
+  <input type="text" name="gender" id="thumbnail"><br>
+  <input type="submit" value="Submit">
+`
+
 function createNewFormEl(roasterId) {
   const formEl = document.createElement('form')
-  const nameEl = document.createElement('input')
-  const thumbnailEl = document.createElement('input')
-  const locationEl = document.createElement('input')
-  const descriptionEl = document.createElement('input')
-  formEl.appendChild(nameEl)
-  formEl.appendChild(thumbnailEl)
-  formEl.appendChild(locationEl)
-  formEl.appendChild(descriptionEl)
+  // const nameEl = document.createElement('input')
+  // const thumbnailEl = document.createElement('input')
+  // const locationEl = document.createElement('input')
+  // const descriptionEl = document.createElement('input')
+  // const nameLabelEl = document.createElement('label')
+  // const thumbnailLabelEl = document.createElement('label')
+  // const locationLabelEl = document.createElement('label')
+  // const descriptionLabelEl = document.createElement('label')
+  // formEl.appendChild(nameEl)
+  // formEl.appendChild(thumbnailEl)
+  // formEl.appendChild(locationEl)
+  // formEl.appendChild(descriptionEl)
   formEl.addEventListener('click', (e) => {e.stopPropagation()})
+  formEl.style.display = 'none'
+  // formEl.onsubmit = 'updateRoaster()'
+  formEl.innerHTML = formHTML
   return formEl
 }
+
+updateRoaster
 
 function getAllRoasters() {
   const url = 'https://twcoffee.herokuapp.com/api/v1/roasters/top'
