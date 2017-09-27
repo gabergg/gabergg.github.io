f4156c6118f8e58621a56e6cc5c5243c623a6c0a
Gabe G'Sell <gabergg@gmail.com>
2017-09-26 23:37:23 -0700

    x

--- thirdwave/assets/javascript/init.js
+++ thirdwave/assets/javascript/init.js
@@ -48,6 +48,7 @@ function updateRoaster(roasterId, e) {
 }
 
 function getAllRoasters() {
+  console.log("WE ALL?")
   const url = 'https://twcoffee.herokuapp.com/api/v1/roasters/all'
   fetch(url).then((response) => {
     response.json().then((results) => {
